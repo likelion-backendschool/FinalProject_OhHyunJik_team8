083ea75ec8233449f71453a2a9384528f24f21dd
hyujikoh <dvum0045@gmail.com>
2022-10-31 15:48:16 +0900

    3주차 기능 개발 [feat] - init Data 생성 1. Order 데이터 생성(결제 및 환불 주문 데이터)

--- 3Week_Mission/src/main/java/com/example/demo/app/base/initData/DevInitData.java
+++ 3Week_Mission/src/main/java/com/example/demo/app/base/initData/DevInitData.java
@@ -5,6 +5,7 @@ import com.example.demo.app.cart.service.CartService;
 import com.example.demo.app.cash.service.CashService;
 import com.example.demo.app.keyword.service.KeywordService;
 import com.example.demo.app.member.service.MemberService;
+import com.example.demo.app.order.service.OrderService;
 import com.example.demo.app.post.service.PostService;
 import com.example.demo.app.product.service.ProductService;
 import org.springframework.boot.CommandLineRunner;
@@ -17,9 +18,9 @@ import org.springframework.context.annotation.Profile;
 public class DevInitData implements InitDataBefore {
     @Bean
     CommandLineRunner initData(
-            MemberService memberService, PostService postService, ProductService productService, KeywordService keywordService, CartService cartService, CashService cashService) {
+            MemberService memberService, PostService postService, ProductService productService, KeywordService keywordService, CartService cartService, CashService cashService, OrderService orderService) {
         return args -> {
-            before(memberService,postService,productService,keywordService,cartService, cashService);
+            before(memberService,postService,productService,keywordService,cartService, cashService,orderService);
         };
     }
 }
--- 3Week_Mission/src/main/java/com/example/demo/app/base/initData/TestInitData.java
+++ 3Week_Mission/src/main/java/com/example/demo/app/base/initData/TestInitData.java
@@ -4,6 +4,7 @@ import com.example.demo.app.cart.service.CartService;
 import com.example.demo.app.cash.service.CashService;
 import com.example.demo.app.keyword.service.KeywordService;
 import com.example.demo.app.member.service.MemberService;
+import com.example.demo.app.order.service.OrderService;
 import com.example.demo.app.post.service.PostService;
 import com.example.demo.app.product.service.ProductService;
 import org.springframework.boot.CommandLineRunner;
@@ -16,9 +17,9 @@ import org.springframework.context.annotation.Profile;
 public class TestInitData implements InitDataBefore {
     @Bean
     CommandLineRunner initData(
-            MemberService memberService, PostService postService, ProductService productService, KeywordService keywordService, CartService cartService, CashService cashService) {
+            MemberService memberService, PostService postService, ProductService productService, KeywordService keywordService, CartService cartService, CashService cashService, OrderService orderService) {
         return args -> {
-            before(memberService,postService,productService,keywordService,cartService, cashService);
+            before(memberService,postService,productService,keywordService,cartService, cashService,orderService);
         };
     }
 }
